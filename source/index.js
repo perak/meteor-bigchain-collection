aa898a40d318ba28e0250ba3a79770cf404a6392
Petar Korponaić <petar.korponaic@gmail.com>
2017-10-04 13:40:55 +0200

    Removed source from .gitignore

---
+++ source/index.js
@@ -0,0 +1,54 @@
+var driver = null;
+var conn = null;
+var settings = null;
+
+export class BigchainCollection extends Mongo.Collection {
+	constructor(name, options) {
+		super(name, options);
+	}
+
+	insert(doc, callback) {
+		var self = this;
+
+
+		if(Meteor.isServer) {
+			settings = settings || Meteor.settings.bigchain || { url: "", publicKey: "", privateKey: "" };
+			driver = driver || require('bigchaindb-driver');
+			conn = conn || new driver.Connection(settings.url);
+
+
+			var payload = JSON.parse(JSON.stringify(doc));
+
+			doc._transactionId = null;
+			doc._transactionStatus = "pending";
+			super.insert(doc, function(e, r) {
+				if(e) {
+					throw e;
+				}
+				if(callback) {
+					callback(e, r);
+				}
+
+				// Construct a transaction payload
+				payload._id = r;
+				const tx = driver.Transaction.makeCreateTransaction(
+					payload, 
+					null,
+					[ driver.Transaction.makeOutput(driver.Transaction.makeEd25519Condition(settings.publicKey))],
+					settings.publicKey
+				);
+
+				const txSigned = driver.Transaction.signTransaction(tx, settings.privateKey);
+
+				conn.postTransaction(txSigned).then(function() { 
+					conn.pollStatusAndFetchTransaction(txSigned.id).then(function(retrievedTx) {
+						self.update({ _id: r }, { $set: { _transactionId: retrievedTx.id, _transactionStatus: "ok" } });
+					});
+				});
+			});
+
+		} else {
+			super.insert.apply(this, arguments);
+		}
+	}
+}
